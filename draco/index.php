8aaf428d6389802180356820ca8e87b6cb2d47c4
samuel <damosisamuel@gmail.com>
2023-02-16 15:05:37 +0100

    index & styles

---
+++ draco/index.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="styles.css">
+    <!-- link bootsrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+     <!-- font awesome link -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <!-- header starts here -->
+    <header>
+        <div class="row p-2 overlay">
+            <div class="offset-1 col-2">
+                <img style="height:50px;" src="log1.svg" alt="">
+
+            </div>
+            <div class="col-8 ">
+                <ul class="d-flex justify-content-around list-unstyled myowntext pt-2">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Testimonial</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                    <li><button class="com mb-2">Components</button></li>
+                </ul>
+
+            </div>
+
+            <div class="row">
+                <div class="offset-1 col-4 juhu">
+                    <h2>We Make Futuristic Items </h2>
+                    <button class="lm mt-3">Learn More</button>
+    
+                </div>
+    
+            </div>
+            <div class="row bgyu mt-4">
+                <div class="offset-1 col-4">
+                    <small>Are locking for</small>
+                    <h5>Lorem Inpsum</h5>
+                    <p>Lorem ipsum dolor amet, consecteur adipsing elit</p>
+
+                </div>
+                <div class="offset-1 col-3 mt-4">
+                    <ul class="list-unstyled">
+                        <li><i class="fa-solid fa-duotone fa-microphone-lines p-2"></i> (123) 456-7890 </li>
+                        <li><i class="fa-regular fa-envelope p-2"></i>info@domain.com</li>
+                    </ul>
+
+                </div>
+                <div class="col-1 mt-4">
+                    <p>Social Media</p>
+                    <ul class="d-flex list-unstyled justify-content-around">
+                        <li><i class="fa-regular fa-brands fa-facebook"></i></li>
+                        <li><i class="fa-regular fa-brands fa-instagram"></i></li>
+                        <li><i class="fa-regular fa-brands fa-google"></i></li>
+                        <li><i class="fa-regular fa-brands fa-pinterest"></i></li>
+
+
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+      
+
+    </header>
+    <!-- header ends here -->
+    <!-- main starts here -->
+    <main class="mt-4 " >
+        <div class="row lock">
+            <div class="offset-1 col-3">
+
+                <small style="color: #a6abb0; font-weight: 800;">nobis dolorem sapiente evenie</small>
+                <h4 class="pt-4 mb-3">About Us</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum sunt, unde aperiam aliquid quia repudiandae, ex harum quis amet delectus maxime, tempora possimus aut laboriosam magni corrupti labore. Doloremque, sit?</p>
+
+                <p style="color: #ff4545;">Read more ..</p>
+            </div>
+            <div class="offset-1 pt-4 col-3 ">
+                <div class="row ">
+                    <div class="col-2"><i class="fa-regular fa-calendar myicon"></i></div>
+                    <div class="col-7">
+                        <ul class="list-unstyled">
+                            <li class="jerry">15+</li>
+                            <li>onsecteur perspiciatis</li>
+                        </ul>
+                    </div>
+
+
+                </div>
+                <div class="row mt-5">
+                    <div class="col-2"><i class="fa-regular fa-face-smile myicon"></i></div>
+                    <div class="col-7">
+                        <ul class="list-unstyled">
+                            <li class="jerry">125+</li>
+                            <li>onsecteur perspiciatis</li>
+                        </ul>
+                    </div>
+
+
+                </div>
+
+            </div>
+            <div class="col-3">
+
+                <div class="row pt-4">
+                    <div class="col-2 "><i class="fa-regular fa-star myicon "></i></div>
+                    <div class="col-7">
+                        <ul class="list-unstyled">
+                            <li class="jerry ">3434+</li>
+                            <li>onsecteur perspiciatis</li>
+                        </ul>
+                    </div>
+
+
+                </div>
+                <div class="row mt-5">
+                    <div class="col-2"><i class="fa-solid fa-user-secret myicon"></i></div>
+                    <div class="col-7">
+                        <ul class="list-unstyled">
+                            <li class="jerry">15+</li>
+                            <li>onsecteur perspiciatis</li>
+                        </ul>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+        </div>
+        <section class="sectOne">
+            <div class="row">
+                <div class="offset-1 col-3 mt-4">
+                    <p style="color: #a6abb0; font-weight: 800;">Blanditiss unde illum earum</p>
+                    <h4>Expertises</h4>
+
+                </div>
+
+            </div>
+
+            <div class="row text-center mt-4">
+                <div class="offset-1 col-3" >
+                    <img class="img-fluid" src="pic1.jpeg" alt="">
+
+                    <div class="mydi">
+                        <h5>Coding & Designing</h5>
+                    <p>Lorem ipsum dolor sit amet, Consectetur adipsicing elit</p>
+
+                    </div>
+                    
+
+                </div>
+                <div class="col-3">
+                    <img class="img-fluid" src="pic2.jpeg" alt="">
+                    <div class="mydi">
+                        <h5>Graphic & Design</h5>
+                        <p>Lorem ipsum dolor sit amet, Consectetur adipsicing elit</p>
+
+                    </div>
+                   
+
+                </div>
+                <div class="col-3">
+                    <img class="img-fluid" src="pic3.jpeg" alt="">
+                    <div class="mydi">
+                        <h5>Development</h5>
+                        <p>Lorem ipsum dolor sit amet, Consectetur adipsicing elit</p>
+
+                    </div>
+                   
+
+                </div>
+
+            </div>
+
+        </section>
+        <section class="grade">
+            <div class="row">
+                <div class="offset-1 col-3 mt-4">
+                    <p style="color: #a6abb0; font-weight: 800;">Culpa perferendis excepturi.</p>
+                    <h4>Our Products</h4>
+
+                </div>
+
+            </div>
+            <div class="row container-fluid mt-3 ">
+                <div class="col-3  text-center">
+                    <img class="img-fluid" src="mac1.jpeg" alt="">
+                    <div class="mydi">
+                        <h5 style="color: #E91F1A;">Fuga asperiores</h5>
+                        <p>Lorem ipsum dolor sit amet, Consectetur adipsicing elit</p>
+
+                    </div>
+                   
+
+                </div>
+                <div class="col-3 text-center">
+                    <img class="img-fluid" src="mac2.jpeg" alt="">
+                    <div class="mydi">
+                        <h5 style="color: #E91F1A;">Voluptatibus iure!</h5>
+                        <p>Lorem ipsum dolor sit amet, Consectetur adipsicing elit</p>
+
+                    </div>
+                   
+
+                </div>
+
+                <div class="col-3 text-center">
+                    <img class="img-fluid" src="mac3.jpeg" alt="">
+                    <div class="mydi">
+                        <h5 style="color: #E91F1A;">Autem minus animi</h5>
+                        <p>Lorem ipsum dolor sit amet, Consectetur adipsicing elit</p>
+
+                    </div>
+                   
+
+                </div>
+
+                <div class="col-3 text-center">
+                    <img class="img-fluid" src="mac4.jpeg" alt="">
+                    <div class="mydi">
+                        <h5 style="color: #E91F1A;">Sed eligendi</h5>
+                        <p>Lorem ipsum dolor sit amet, Consectetur adipsicing elit</p>
+
+                    </div>
+                   
+
+                </div>
+
+                <div class="row">
+                    <div class="offset-1 col-3 mt-4">
+                        <p style="color: #a6abb0; font-weight: 800;">Culpa perferendis excepturi.</p>
+                        <h4>Contact Us</h4>
+    
+                    </div>
+    
+                </div>
+
+                        <div class="row  mt-5 ">
+                            <div class="offset-1 col-7">
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="Name" aria-label="Username">
+                                    <input type="text" class="form-control" placeholder="Email" aria-label="Server">
+                                  </div>
+                                    <div class="form-floating">
+                                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                                        <label for="floatingTextarea2">Your Message</label>
+                                        
+                                      </div>
+                                      <div class=" d-grid gap-2 mt-4">
+                                          <button class="btn btn-primary" style="background-color: #ED3E3E; border: 1px solid #ED3E3E;" type="button">Send Message</button>
+
+                                      </div>
+
+
+
+                            </div>
+                            <div class="col-4">
+                                <div class="ci">
+                                    <h4>CONTACT INFO</h4>
+                                    <hr style="width: 50%;">
+                                    <p>Contacts us and we'll get back to you within 24hours</p>
+                                    <p> <i class="fa-solid fa-location p-1"></i> 12345 Fake St Nowhere NY</p>
+                                    <p><i class="fa-solid fa-envelope p-1"></i>infp@domain.com</p>
+                                    <p><i class="fa-solid fa-phone p-1"></i>(345) 908 9808</p>
+
+                                    
+                                </div>
+
+                            </div>
+                           
+
+                        </div>
+
+                 
+
+                
+
+
+
+
+            </div>
+        </section>
+        <hr>
+        <div class="row">
+            <div class="offset-1 col-4">
+                <p>Copyright 2045 © Samuel Gbadamosi  </p>
+
+            </div>
+            <div class="offset-2 col-4">
+                <ul class="d-flex justify-content-around list-unstyled"> 
+                    <li><i class="fa-solid fa-brands fa-facebook"></i></li>
+                    <li><i class="fa-solid fa-brands fa-twitter "></i></li>
+                    <li><i class="fa-solid fa-brands fa-google"></i></li>
+                    <li><i class="fa-solid fa-brands fa-instagram"></i></li>
+                </ul>
+
+            </div>
+
+        </div>
+        
+    </main>
+    <!-- main ends here -->
+    <!-- footer starts here -->
+    <footer>
+
+    </footer>
+    <!-- footer ends here -->
+
+    
+</body>
+</html>
